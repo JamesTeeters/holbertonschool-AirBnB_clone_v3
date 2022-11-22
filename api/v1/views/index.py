fd9fc6404242e55c96eecde61d87e3ad0522a244
GoldLion1011 <yogirose79@gmail.com>
2022-11-22 13:32:48 -0800

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """check status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def stats():
+    """ """
+    count_directory = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(count_directory)
+
