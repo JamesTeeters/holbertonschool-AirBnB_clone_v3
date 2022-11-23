327e25f5eef8d57d8d69f2dc27a0c7af9eb418b7
GoldLion1011 <yogirose79@gmail.com>
2022-11-23 09:06:57 -0800

    index, appy

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
@@ -22,4 +23,11 @@ def error_not_found(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    host = '0.0.0.0'
+    port = '5000'
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+
+    app.run(host=host, port=port, threaded=True)
