0bb351bba52989600a382732ff42fa4b2ed88488
GoldLion1011 <yogirose79@gmail.com>
2022-11-23 08:46:38 -0800

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Documentation """
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """ closes current sesssion """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_not_found(exception):
+    """ 404 page not found error handler """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
