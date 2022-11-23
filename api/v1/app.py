aef86cd1016818b892cedc4a59eacdf27947ef99
GoldLion1011 <yogirose79@gmail.com>
2022-11-23 09:38:12 -0800

    port host

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from os import environ
 
 
 app = Flask(__name__)
@@ -23,11 +23,7 @@ def error_not_found(exception):
 
 
 if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = '5000'
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
+    host = environ.get('HBNB_API_HOST','0.0.0.0')
+    port = environ.get('HBNB_API_PORT', '5000')
 
     app.run(host=host, port=port, threaded=True)
