4557fcfdef9ce9454a7de8b9102a01d5dd0b87b0
GoldLion1011 <yogirose79@gmail.com>
2022-11-28 09:45:00 -0800

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import environ
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
 
 
@@ -23,7 +25,7 @@ def error_not_found(exception):
 
 
 if __name__ == '__main__':
-    host = environ.get('HBNB_API_HOST','0.0.0.0')
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
     port = environ.get('HBNB_API_PORT', '5000')
 
     app.run(host=host, port=port, threaded=True)
