5c963c329058777eaf89976798bfba6d2661977f
JamesTeeters <4925@holbertonstudents.com>
2022-11-28 17:52:08 -0800

    pep8

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -51,7 +51,8 @@ def new_review(place_id):
     try:
         req_dict = request.get_json(silent=True)
         if req_dict is not None:
-            if 'user_id' in req_dict.keys() and req_dict['user_id'] is not None:
+            if 'user_id' in req_dict.keys()
+            and req_dict['user_id'] is not None:
                 if 'text' in req_dict.keys() and req_dict['text'] is not None:
                     req_dict['place_id'] = place_id
                     new_review = Review(**req_dict)
